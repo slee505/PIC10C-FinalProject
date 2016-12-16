14651f7d0c0c472e644f065503b29a11d94f41f8
slee505 <stephen.kr.lee@gmail.com>
2016-12-15 20:32:09 -0800

    Definitions for scoring system

--- score.cpp
+++ score.cpp
@@ -2,16 +2,20 @@
 
 Score::Score(QGraphicsItem *Parent)
 {
+    //set score to 0
     score = 0;
+    //write score out as text
     setPlainText(QString::number(score));
 }
 
 void Score::increase()
 {
+    //increase score by 100
     score += 100;
     setPlainText(QString::number(score));
 }
 
+//getter for score value
 int Score::getScore()
 {
     return score;
