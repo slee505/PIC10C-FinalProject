b32fa3ad6e670318b457657badae95bf997acfde
slee505 <stephen.kr.lee@gmail.com>
2016-12-15 20:38:16 -0800

    Commented

--- snake.h
+++ snake.h
@@ -17,20 +17,32 @@ class Snake : public QObject, public QGraphicsRectItem{
         Q_OBJECT
 public:
     Snake();
+    //defines behavior for arrow key presses
     void keyPressEvent(QKeyEvent * event);
+    //increase snake size
     void grow();
+    //keeps snake moving
     void move();
+    //disconnects movement slots
     void disconnectAll();
+    //checks for collision with snake body
     void checkForCollision();
+    //checks for collision with wall
     void WallCollision();
+    //defines behavior for eating food
     void eat();
+        
+    //int values for previous positions
     int PrevX;
     int PrevY;
 private:
+    //list of positions of snake's body
     QList<snakeBody*> body;
+    //set starting direction to right
     std::string currentDirection = "right";
     QTimer * timer = new QTimer;
 public slots:
+    //directional slots
     void moveRight();
     void moveLeft();
     void moveUp();
